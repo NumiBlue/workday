13a2c8424a6ff4a5fb36692b2dcc49b01ae99e92
NumiBlue <thekindlyblog@gmail.com>
2022-02-01 09:22:22 -0600

    js and pseudocode

---
+++ script.js
@@ -0,0 +1,8 @@
+//Pseudocode:
+//Show day at top using Moment.js
+//Create Input areas for each hour
+//Use js to influnce the css colors according to current time
+//Click enable typing
+//Click enable save button
+//Save button saves to local storage
+//Local storage loads events when refreshed
