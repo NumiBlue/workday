91ccbe2239062cab71e6f7b5ef65451427dfe0d0
NumiBlue <thekindlyblog@gmail.com>
2022-02-27 13:43:35 -0600

    fixes 8 and 9

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 //Show day at top using Moment.js
 //Create Input areas for each hour
 //Use js to influnce the css colors according to current time
+//-get current time, get syntax for changing according to current time- military time.. for loop
 //Click enable typing
-//Click enable save button
-//Save button saves to local storage
-//Local storage loads events when refreshed
+//Click enable save button (universal listener, apply to .btn)
+//Save button saves to local storage 
+//Local storage loads events when refreshed (key as time, value as string, use for loop)
