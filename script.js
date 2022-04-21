ff126df0544d82293d1da0a28c36cab22c3c0f12
NumiBlue <thekindlyblog@gmail.com>
2022-04-21 00:14:53 -0500

    displays day

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 //Pseudocode:
-//Show day at top using Moment.js
-//Create Input areas for each hour
+
+
 //Use js to influnce the css colors according to current time
 //-get current time, get syntax for changing according to current time- military time.. for loop
-//Click enable typing
 //Click enable save button (universal listener, apply to .btn)
 //Save button saves to local storage 
 //Local storage loads events when refreshed (key as time, value as string, use for loop)
+
+//Moment
+const dateTime = moment()
+$("#moment").text(dateTime);
+
+//Save Button
+$("#btn08").click(function() {
+    var text = $('textarea#text08').val();
+    console.log(text);
+    //send to server and process response
+});
